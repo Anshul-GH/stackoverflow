1a4338c3a0dc53eda5e75e21c55b8d35a63801c8
Anshul <samgalacestyle01@gmail.com>
2020-05-28 01:55:49 +0530

    Routine commit

---
+++ 62050988.py
@@ -0,0 +1,20 @@
+'''
+brand        model      year       engine    transmission   suspension   brakes   Overall_Score
+Acura        CL         1999          3           2           1           3          1
+Acura        CL         2000          5           3           7           2          3
+BMW          520        2015          22          4           10          11         2
+BMW          520        2008          1           6           3           6          4
+Chevrolet    Impala     1997          0           2           2           5          1
+Chevrolet    Impala     2002          9           3           8           4          1
+'''
+
+import pandas as pd
+
+# df = pd.read_clipboard()
+
+# df.set_index('brand', inplace=True)
+
+# df.to_json('62050988.json', orient='index')
+
+# print(df)
+
