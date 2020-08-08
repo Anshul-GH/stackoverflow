796d05aae6ec240341a9027abffaac630ddcd7ac
Anshul <samgalacestyle01@gmail.com>
2020-08-09 00:59:00 +0530

    Routine Commit

---
+++ icertis.py
@@ -0,0 +1,17 @@
+# # class Parent:
+# #     def __init__(self, name):
+# #         self.name1 = name
+
+
+# # class Child(Parent):
+# #     def __init__(self, name):
+# #         self.name2 = name
+
+
+# # temp = Child("Alice")
+# # print("%s %s "%(temp.name1, temp.name2))
+
+# string = "my name is z"
+
+# for i in string:
+#     print(i)
